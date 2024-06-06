d5fc944489ab6240db641c4dbb6c2a44afe19b4e
StoneySpring688 <66467256+StoneySpring688@users.noreply.github.com>
2024-06-06 17:27:07 +0200

    Add files via upload
    cambios :
    se implementó un analizador léxico básico

---
+++ src/lex.h
@@ -0,0 +1,20 @@
+/*.h lexico*/
+
+#define             ID          1
+#define             NUM         2
+#define             SUMA        3
+#define             REST        4
+#define             MULT        5
+#define             DIV         6
+#define             IGUA        7
+#define             PYCO        8
+#define             PRINT       9
+#define             MAIN        10
+#define             PARI        11
+#define             PARD        12
+#define             LLAI        13
+#define             LLAD        14
+#define             COMA        15
+#define             CADENA      16
+
+
